7dd35f131c7a5848dc7d2ffd72d616159355a49f
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-05-12 22:17:37 -0300

    arquivos base e js funcional

---
+++ index.js
@@ -0,0 +1,33 @@
+let today = new Date();
+
+$("#btn").click(requisicao);
+
+function requisicao() {
+    const pegarData = $("#calendario").val();
+
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=w4otrhmBtewMkhK8LzVqzzwVnXUzjjI9T3ehDcNq&date=${pegarData}`,
+        success: function (resultado) {
+            if (resultado.media_type == 'image') {
+                document.getElementById("media").innerHTML = "<img src='" + resultado.url + "'style='width:50%;'/>";
+            } else if (resultado.media_type == 'video') {
+                document.getElementById("media").innerHTML = "<iframe src=" + resultado.url + "></iframe>";
+            }
+            if (resultado.copyright == undefined) {
+                document.getElementById("copyright").innerHTML = "";
+            } else {
+                document.getElementById("copyright").innerHTML = "By " + resultado.copyright;
+            }
+            document.getElementById("title").innerHTML = resultado.title;
+            document.getElementById("explanation").innerHTML = resultado.explanation;
+            console.log(resultado);
+        },
+        error: function (erro) {
+            console.log(erro);
+        }
+
+    });
+}
+
+
+
